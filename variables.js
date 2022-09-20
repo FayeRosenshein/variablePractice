df972495d1d105e29f7a4955a748463bda1e21a6
Faye Rosenshein <rosefaye.a@gmail.com>
2022-09-20 14:21:58 -0600

    Print out everything

--- variables.js
+++ variables.js
@@ -6,6 +6,20 @@ var age = 27
 var studentId = 2022000247
 var averageSalary = 80000
 
+var inColorado = false
+var hasPets = false
+var canType = false
+
 var inColorado = true
 var hasPets = true
 var canType = true
+
+console.log(name)
+console.log(email)
+console.log(school);
+console.log(age);
+console.log(studentId);
+console.log(averageSalary);
+console.log(inColorado);
+console.log(hasPets);
+console.log(canType);
